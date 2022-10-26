52c899bc53cb17b43e5438b27ce4374756b0776b
cmzcasino1 <cmcasino1@hotmail.com>
2022-10-26 22:32:52 +0700

    second commit

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          เว็บตรง ระบบฝากถอนอัตโนมัติ 24 ชม.
         </p>
 
       </main>
